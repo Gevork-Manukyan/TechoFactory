825679a227b560d9c4907dc218f5d2f09cd9358b
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-11-28 21:38:31 -0800

    adds animation to plus button

--- src/Components/BluePrints/BluePrint/BluePrint.js
+++ src/Components/BluePrints/BluePrint/BluePrint.js
@@ -1,12 +1,14 @@
 import ImageBluePrint from "../ImageBluePrint/ImageBluePrint"
+import PlusButton from "../PlusButton/PlusButton"
 import TextBluePrint from "../TextBluePrint/TextBluePrint"
 import "./BluePrint.css"
 
 export default function BluePrint() {
+
+
     return (
         <div className="BluePrint">
-            {/* <ImageBluePrint /> */}
-            <TextBluePrint />
+            <PlusButton />
         </div>
     )
 }
---
+++ src/Components/BluePrints/PlusButton/PlusButton.js
@@ -0,0 +1,10 @@
+import "./PlusButton.css"
+import icon from "../../../assets/blueprint/plus-icon.png"
+
+export default function PlusButton() {
+    return(
+        <div className="PlusButton">
+            <img src={icon} />
+        </div>
+    )
+}
