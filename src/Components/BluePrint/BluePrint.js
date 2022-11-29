8e67791be78ec44ca909ea164fac28a6d59eb638
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-11-28 20:00:52 -0800

    creates ImageBluePrint

--- src/Components/BluePrint/BluePrint.js
+++ src/Components/BluePrint/BluePrint.js
@@ -1,9 +1,10 @@
+import ImageBluePrint from "../ImageBluePrint/ImageBluePrint"
 import "./BluePrint.css"
 
 export default function BluePrint() {
     return (
         <div className="BluePrint">
-            
+            <ImageBluePrint />
         </div>
     )
 }
---
+++ src/Components/ImageBluePrint/ImageBluePrint.js
@@ -0,0 +1,10 @@
+import "./ImageBluePrint.css"
+import icon from "../../assets/image-icon.png"
+
+export default function ImageBluePrint() {
+    return (
+        <div className="ImageBluePrint">
+            <img src={icon} />
+        </div>
+    )
+}
