bdd89adf7526631ff2b5f942946adedccd20e145
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-11-28 20:16:07 -0800

    creates text blueprint

--- src/Components/BluePrint/BluePrint.js
+++ src/Components/BluePrint/BluePrint.js
@@ -1,10 +1,12 @@
 import ImageBluePrint from "../ImageBluePrint/ImageBluePrint"
+import TextBluePrint from "../TextBluePrint/TextBluePrint"
 import "./BluePrint.css"
 
 export default function BluePrint() {
     return (
         <div className="BluePrint">
-            <ImageBluePrint />
+            {/* <ImageBluePrint /> */}
+            <TextBluePrint />
         </div>
     )
 }
---
+++ src/Components/TextBluePrint/TextBluePrint.js
@@ -0,0 +1,10 @@
+import "./TextBluePrint.css"
+import icon from "../../assets/blueprint/text-icon.png"
+
+export default function TextBluePrint() {
+    return (
+        <div className="TextBluePrint">
+            <img src={icon} />
+        </div>
+    )
+}
